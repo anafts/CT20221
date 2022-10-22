8ebf3da97ecb8c3a3f8dbc8b75613f81c8087b8f
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-22 12:09:14 -0300

    database config

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    database: 'kdramas',
+    username: 'postgres',
+    password: null,
+    define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
