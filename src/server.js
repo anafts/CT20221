4590d779a7c669ee770826b30b2448d17ca8e2d3
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-22 19:03:23 -0300

    server updated

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const routes = require("./routes");
 
+const db = require('./database/index');
+
+// db.sync({ force: true });
+
 const app = express();
 
-app.use(routes);
 app.use(express.json());
+app.use(routes);
+
 
 app.listen(8888, () => {
-    console.log("Server is ruinnig")
+    console.log("Server is running")
 });
