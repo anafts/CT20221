72462983ba6a59fee7211739de1c3b284195d523
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-22 19:04:41 -0300

    kdrama put, delete, get and post routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const UserController = require('./controllers/KdramaController');
 
 const routes = express.Router();
 
-routes.get('/', (res, req) => {
-    return res.json({
-         message: " hello world",
-    })
-});
+routes.get('/kdrama', UserController.index );
+routes.get('/kdrama/:id', UserController.findById );
+routes.post('/kdrama', UserController.store );
+routes.delete('/kdrama/:id', UserController.removeById );
+routes.put('/kdrama/:id', UserController.updateById );
+
+
 
 module.exports = routes;
