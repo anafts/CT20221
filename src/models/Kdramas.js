bd70aa08258933969d5ed46eded28cbd55f343a1
Ana Freitas <ana.freitaas16@gmail.com>
2022-11-07 18:15:30 -0300

    added genres, review and rating to Kdrama model

--- src/models/Kdramas.js
+++ src/models/Kdramas.js
@@ -44,6 +44,35 @@ class Kdramas extends Model {
                     },
                 }
             },
+            review: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                len: {
+                    args: [0, 500],
+                    // msg: {
+                    //     notEmpty: "this field must have a maximum of 500 characters"
+                    // }
+                }
+            },
+            rating: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+                validate: {
+                    max: 5.0,
+                    min: 0.0,
+                    // msg: {
+                    //     notEmpty: "it should be rated between 0.0 and 5.0"  
+                    // }
+                },
+                defaultValue: 0.0
+
+            },
+            genres: {
+                type: DataTypes.ENUM,
+                allowNull: true,
+                values: ["romance", "action", "romantic comedy", "others"],
+                defaultValue: "others"
+            }
 
         }, {
             sequelize,
