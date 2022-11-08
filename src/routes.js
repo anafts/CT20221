a894d7e9f02afbb0b84b8f2663569c4f23d28e0a
Ana Freitas <ana.freitaas16@gmail.com>
2022-11-08 09:31:56 -0300

    created summary router

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const KdramaController = require('./controllers/KdramaController');
+const SummaryController = require('./controllers/SummaryController');
 
 const routes = express.Router();
 
@@ -8,6 +9,8 @@ routes.post('/kdrama', KdramaController.store );
 routes.get('/kdrama/:id', KdramaController.findById );
 routes.delete('/kdrama/:id', KdramaController.removeById );
 routes.put('/kdrama/:id', KdramaController.updateById );
+
+routes.get('/summary', SummaryController.index);
 
 
 module.exports = routes;
