8648180f1df6f455be508fe29948c99191ff75e5
Ana Freitas <ana.freitaas16@gmail.com>
2022-11-26 10:57:36 -0300

    add secret keys

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
+const dotenv = require("dotenv");
+
+dotenv.config()
+
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    database: 'kdramas',
-    username: 'postgres',
-    password: null,
+    host: process.env.SECRET_HOST,
+    database: process.env.SECRET_BANK_NAME,
+    username: process.env.SECRET_BANK_USER,
+    password: process.env.SECRET_BANK_PASSWORD,
     define: {
     timestamps: true,
     underscored: true,
