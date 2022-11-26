9154ec180c499760c9168ed5e58e37b2130f69da
Ana Freitas <ana.freitaas16@gmail.com>
2022-11-26 09:12:27 -0300

    added users model

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,11 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 const Kdramas = require('../models/Kdramas');
+const Users = require('../models/Users');
 
 const connection = new Sequelize(dbConfig);
 
 Kdramas.init(connection);
+Users.init(connection);
 
 module.exports = connection;
