f05bbd46c2192988a74cea522184ca568474d3f9
Ana Freitas <ana.freitaas16@gmail.com>
2022-11-28 09:18:05 -0300

    kdrama table

--- src/database/migrations/20221022144224-create-kdramas_names.js
+++ src/database/migrations/20221022144224-create-kdramas_names.js
@@ -1,46 +1,43 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return await queryInterface.createTable('kdramas_names', 
-    {
-       id:{
-       type: Sequelize.INTEGER,
-       primaryKey: true,
-       autoIncrement: true,
-       allowNull: false,
+  async up(queryInterface, Sequelize) {
+    return await queryInterface.createTable("kdramas_names", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
       },
       drama_name: {
-      type: Sequelize.STRING,
-      allowNull: false,
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       favorite_character: {
-      type: Sequelize.STRING,
-      allowNull: false,
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       streamming_services: {
-      type: Sequelize.STRING,
-      allowNull: false,
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       release_year: {
-      type: Sequelize.STRING,
-      allowNull: false,
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       created_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-      }, 
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
       updated_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-     }
-
-   });
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    
-    return await queryInterface.dropTable('kdramas_names');
-  }
+  async down(queryInterface, Sequelize) {
+    return await queryInterface.dropTable("kdramas_names");
+  },
 };
